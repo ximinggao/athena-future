69df004b4c9dd09ed365ee02d403c0bb8dd1a978
Brook Xi <minggao.xi@gmail.com>
2015-10-20 17:35:33 +0800

    Add edge server and service-discovery server

---
+++ support/edge/src/main/java/com/athena/edge/EdgeApplication.java
@@ -0,0 +1,14 @@
+package com.athena.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EdgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeApplication.class, args);
+    }
+}
