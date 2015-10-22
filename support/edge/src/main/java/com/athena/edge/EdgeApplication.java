4cd01b6928c038a8ee75df268ff411d01a503e93
Brook Xi <minggao.xi@gmail.com>
2015-10-22 21:08:43 +0800

    Set application name for each service

--- support/edge/src/main/java/com/athena/edge/EdgeApplication.java
+++ support/edge/src/main/java/com/athena/edge/EdgeApplication.java
@@ -2,10 +2,11 @@ package com.athena.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableEurekaClient
 public class EdgeApplication {
 
     public static void main(String[] args) {
