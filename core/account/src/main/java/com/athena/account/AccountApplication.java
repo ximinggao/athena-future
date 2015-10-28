012d9897125270b17388737a7151e24ddb10b2ad
Brook Xi <minggao.xi@gmail.com>
2015-10-28 18:10:55 +0800

    Make edge server work with zuul

--- core/account/src/main/java/com/athena/account/AccountApplication.java
+++ core/account/src/main/java/com/athena/account/AccountApplication.java
@@ -3,12 +3,25 @@ package com.athena.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class AccountApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
+    }
+
+    public static String DNA = UUID.randomUUID().toString();
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Hello World from Account: " + DNA;
     }
 }
--- support/edge/src/main/java/com/athena/edge/EdgeApplication.java
+++ support/edge/src/main/java/com/athena/edge/EdgeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableZuulProxy
 public class EdgeApplication {
 
     public static void main(String[] args) {
