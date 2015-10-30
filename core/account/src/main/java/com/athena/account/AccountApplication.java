2c36c42645925d87aeeada70e4d8c672990312c0
Brook Xi <minggao.xi@gmail.com>
2015-10-30 17:57:21 +0800

    Enable session cluster backed by Redis

--- core/account/src/main/java/com/athena/account/AccountApplication.java
+++ core/account/src/main/java/com/athena/account/AccountApplication.java
@@ -3,6 +3,7 @@ package com.athena.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 @EnableEurekaClient
 @RestController
 public class AccountApplication {
