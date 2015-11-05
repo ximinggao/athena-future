c83701b0104589226655da6db6dbee4764643f71
Brook Xi <minggao.xi@gmail.com>
2015-11-05 17:17:52 +0800

    Dockerize the project

--- support/edge/src/main/java/com/athena/edge/EdgeApplication.java
+++ support/edge/src/main/java/com/athena/edge/EdgeApplication.java
@@ -2,16 +2,26 @@ package com.athena.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @SpringBootApplication
 @EnableRedisHttpSession
 @EnableZuulProxy
+@RestController
 public class EdgeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EdgeApplication.class, args);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Hello World: " + new Date().toString();
     }
 }
